faea16403899231b034d844555f2c83c758b8026
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-01-09 11:39:38 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,15 +80,24 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event, ui) {
     console.log(ui);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event, ui) {
     console.log(ui);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event) {
     console.log(event);
+    $(this).addClass('dropover-active');
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event) {
     console.log(event);
+    $(this).removeClass('dropover-active');
+    $('.bottom-trash').removeClass('bottom-trash-active');
+
   },
   update: function() {
     var tempArr = [];
@@ -159,7 +168,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -262,6 +271,7 @@ $(".list-group").on("change", "input[type='text']", function() {
     .closest(".list-group-item")
     .index();
 
+  
   // update task in array and re-save to localstorage
   tasks[status][index].date = date;
   saveTasks();
@@ -284,5 +294,10 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, ((1000 * 60) * 30));
 // load tasks for the first time
 loadTasks();
